3df5e6a0738e23cdbc6c2a08fce5446839622d57
shaun jacob <sjacobnz86@gmail.com>
2020-12-02 12:50:55 +1300

    some basic library tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,66 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, screen, act, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+ 
+//https://www.robinwieruch.de/react-testing-library
 import App from './App';
+ 
+describe('App', () => {
+  test('renders App component', async () => {
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+       render(<App />) 
+
+    //getBy returns an element or an error.
+    //every time you are asserting that an element isn't there, use queryBy
+    expect(screen.queryByText(/Signed in as/)).toBeNull();
+ 
+  //  screen.debug();
+
+    //The findBy search variant is used for asynchronous elements which will be there eventually.
+    expect(await screen.findByText(/Signed in as/)).toBeInTheDocument();
+
+  //  screen.debug();
+
+
+  });
+});
+
+//you have used two assertive functions: toBeNull and toBeInTheDocument.
+
+describe('App', () => {
+  test('renders App component', async () => {
+    render(<App />);
+ 
+    // wait for the user to resolve
+    // needs only be used in our special case
+    await screen.findByText(/Signed in as/);
+ 
+   // screen.debug();
+   expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
+ 
+    fireEvent.change(screen.getByRole('textbox'), {
+      target: { value: 'JavaScript' },
+    });
+ 
+   // screen.debug();
+   expect(screen.getByText(/Searches for JavaScript/)).toBeInTheDocument();
+  });
+});
+
+
+describe('App', () => {
+  test('renders App component', async () => {
+    render(<App />);
+ 
+    // wait for the user to resolve
+    await screen.findByText(/Signed in as/);
+ 
+    expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
+ 
+    await userEvent.type(screen.getByRole('textbox'), 'JavaScript');
+ 
+    expect(
+      screen.getByText(/Searches for JavaScript/)
+    ).toBeInTheDocument();
+  });
 });
